e0b090411acc9bab0f6f9f8c639c5c891171443b
Luiz Otávio <113857296+luizotavio-amante@users.noreply.github.com>
2025-04-28 20:54:44 -0300

    Add files via upload

---
+++ ExemploAula2.cy.js
@@ -0,0 +1,7 @@
+/// <reference = cypress>
+
+describe("Testes da criação, registro e login", ()=>{
+    ImageTrack("Teste criação de usuario com sucesso", ()=>{
+        cy.visit('https://globalsqa.com/angularJs-protractor/registration-login-example/#/login')
+    })
+})
