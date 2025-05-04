2bd811456b6460d73ef561a5fc180c3089ce066a
Luiz Otávio <113857296+luizotavio-amante@users.noreply.github.com>
2025-05-04 16:01:16 -0300

    Add files via upload

--- ExemploAula2.cy.js
+++ ExemploAula2.cy.js
@@ -1,7 +1,53 @@
 /// <reference = cypress>
 
 describe("Testes da criação, registro e login", ()=>{
-    ImageTrack("Teste criação de usuario com sucesso", ()=>{
+    it.skip("Teste criação de usuario com sucesso", ()=>{
         cy.visit('https://globalsqa.com/angularJs-protractor/registration-login-example/#/login')
+        cy.get('.btn-link').click()
+        cy.get('#firstName').type("Luiz")
+        cy.get('#Text1').type("Luiz")
+        cy.get('#username').type("Luiz")
+        cy.get('#password').type("Luiz")
+        cy.get('.btn-primary').click()
+        cy.get('.ng-binding').should("contain.text", "Registration successful")
+    })
+
+    it.skip("Teste criação de usuario com falha", ()=>{
+        cy.visit('https://globalsqa.com/angularJs-protractor/registration-login-example/#/login')
+        cy.get('.btn-link').click()
+        cy.get('#firstName').type("Luiz")
+        cy.get('#Text1').type("Luiz")
+        cy.get('#username').type("Luiz")
+        cy.get('.btn-primary').should("be.disabled")
+    })
+
+    it("Teste de login de usuario com sucesso", ()=>{
+        let infos = criarUser()
+        cy.visit('https://globalsqa.com/angularJs-protractor/registration-login-example/#/login')
+        cy.get('#username').type(infos[0])
+        cy.get('#password').type(infos[1])
+        cy.get('.btn-primary').click()
+        cy.get('h1.ng-binding').should("contain.text", infos[0])
+
     })
 })
+
+function criarUser(){
+    let hora = new Date().getHours().toString()
+    let minuto = new Date().getMinutes().toString()
+    let seg = new Date().getSeconds().toString()
+    let ID = hora + minuto + seg + "ID"
+    let Senha = hora + minuto + seg + "Senha"
+    let infos = [ID, Senha]
+
+    cy.visit('https://globalsqa.com/angularJs-protractor/registration-login-example/#/login')
+    cy.get('.btn-link').click()
+    cy.get('#firstName').type(ID)
+    cy.get('#Text1').type(ID)
+    cy.get('#username').type(ID)
+    cy.get('#password').type(Senha)
+    cy.get('.btn-primary').click()
+    cy.get('.ng-binding').should("contain.text", "Registration successful")
+    return infos
+      
+}
---
+++ exerciciopraticoaula2.cy.js
@@ -0,0 +1,53 @@
+/// <reference types="cypress" />
+
+describe("Testes da criação, registro, login e exclusão", () => {
+    let infos = []
+
+    beforeEach(() => {
+        // Gerar usuário único por horário
+        let hora = new Date().getHours().toString()
+        let minuto = new Date().getMinutes().toString()
+        let seg = new Date().getSeconds().toString()
+        let ID = hora + minuto + seg + "ID"
+        let Senha = hora + minuto + seg + "Senha"
+        infos = [ID, Senha]
+
+        // Criar usuário
+        cy.visit('https://globalsqa.com/angularJs-protractor/registration-login-example/#/login')
+        cy.get('.btn-link').click()
+        cy.get('#firstName').type(ID)
+        cy.get('#Text1').type(ID)
+        cy.get('#username').type(ID)
+        cy.get('#password').type(Senha)
+        cy.get('.btn-primary').click()
+        cy.get('.ng-binding').should("contain.text", "Registration successful")
+    })
+
+    it("Teste de login com sucesso", () => {
+        cy.visit('https://globalsqa.com/angularJs-protractor/registration-login-example/#/login')
+        cy.get('#username').type(infos[0])
+        cy.get('#password').type(infos[1])
+        cy.get('.btn-primary').click()
+        cy.get('h1.ng-binding').should("contain.text", infos[0])
+    })
+
+    it("Teste de exclusão e falha no login", () => {
+        // Login com usuário criado
+        cy.visit('https://globalsqa.com/angularJs-protractor/registration-login-example/#/login')
+        cy.get('#username').type(infos[0])
+        cy.get('#password').type(infos[1])
+        cy.get('.btn-primary').click()
+
+        // Excluir usuário
+        cy.get('.ng-binding > a').click()
+
+        // Voltar para tela de login e tentar novamente
+        cy.visit('https://globalsqa.com/angularJs-protractor/registration-login-example/#/login')
+        cy.get('#username').type(infos[0])
+        cy.get('#password').type(infos[1])
+        cy.get('.btn-primary').click()
+
+        // Verificar falha no login (não aparece o h1 com o nome)
+        cy.get('.alert-danger').should("contain.text", "Username or password is incorrect")
+    })
+})
